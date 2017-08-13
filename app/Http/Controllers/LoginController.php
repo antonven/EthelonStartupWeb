ef6e646368bd8b18b17855e5c51964c7ebc2c3e6
kobe <kobearelativo@gmail.com>
2017-08-13 23:55:31 +0800

    sige lang tag commit ani

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -35,7 +35,7 @@ class LoginController extends Controller
                                             "api_token"  => $user->api_token,
                                             "message" => $message,
                                             "name"=> $user->name,
-                                            "image_url",$volunteer->image_url)); 
+                                            "image_url" => $volunteer->image_url)); 
                 
             }else{
                 // wa lang sa tay mobile ang foundation
