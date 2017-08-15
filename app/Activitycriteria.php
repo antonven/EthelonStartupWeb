d5c4a8795fb18cf0972f8d6fa3f05c88f2ab86cd
kobe <kobearelativo@gmail.com>
2017-08-16 02:04:17 +0800

    activitycriteria

---
+++ app/Activitycriteria.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Activitycriteria extends Model
+{
+	protected $fillable=['criteria','activity_id'];
+    public $timestamps = false;
+    //
+}
