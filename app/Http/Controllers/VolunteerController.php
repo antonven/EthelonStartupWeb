a6d151746a66f7c23ec7445921a4aa36e1d9992f
kobe <kobearelativo@gmail.com>
2018-01-14 16:59:52 +0800

    asdsd

--- app/Http/Controllers/VolunteerController.php
+++ app/Http/Controllers/VolunteerController.php
@@ -329,4 +329,147 @@ class VolunteerController extends Controller
 
             $new_points = $volunteer_points->points + $sumOfPoints;
 
+            Volunteer::where('volunteer_id',$request->input('volunteer_id'))->update(['points' => $new_points]);
+            \DB::table('activities')->where('activity_id',$request->input('activity_id'))->update(['points_equivalent' => $sumOfPoints]);
+
+            $data = array("result"=>$sumOfPoints);
+            return response()->json($data);            
+            
+    }
+
+
+    public function inputSkills(Request $request){
+      
+
+      $arrays = array();
+      $volunteer_id = $request->input('volunteer_id');
+
+
+        $i = 1;
+
+       
+        if($stringcount = $request->input('count')){
+            $count = (int)$stringcount;
+
+            for($i = 0; $i < $count; $i++){
+
+                 array_push($arrays, $request->input('params'.$i));      
+                  //return response()->json($request->input('params'.$i));
+            }
+
+            
+            foreach($arrays as $value){
+
+                 Volunteerskill::create([
+                  'name' => $value,
+                  'volunteer_id' => $volunteer_id 
+                ]);      
+
+            }
+
+          }
+
+          else{
+
+            return "wtf";
+
+          }
+
+        return "Success";
+    }
+
+    //join activity nga wala pa nahitabo
+    public function joinActivity(Request $request){
+
+       $watch = Volunteeractivity::where('volunteer_id',$request->input('volunteer_id'))
+                                  ->where('activity_id',$request->input('activity_id'))->get();
+
+         if($watch->count()){
+
+            $data = array("message"=>"Already Joined");
+            return response()->json($data);
+            
+         }                                   
+         else{
+
+      
+            Volunteeractivity::create([
+                 'volunteer_id'=>$request->input('volunteer_id'),
+                 'activity_id'=>$request->input('activity_id'),
+                 'status'=> false  
+                ]);
+            
+
+            $data = array("message"=>"Success");
+
+            return response()->json($data);
+            
+            }
+    }
+
     
+    public function points($skill, $sumOfPoints){
+
+
+                    switch($skill){
+
+                         case 'Environmental': $sumOfPoints = $sumOfPoints + 10;
+                                      break;          
+                         case 'Sports': $sumOfPoints = $sumOfPoints + 8;                       
+                                      break;
+                         case 'Culinary': $sumOfPoints = $sumOfPoints + 8;
+                                      break;
+                         case 'Medical': $sumOfPoints = $sumOfPoints + 10;
+                                      break;
+                         case 'Charity': $sumOfPoints = $sumOfPoints + 10;
+                                      break;
+                         case 'Livelihood': $sumOfPoints = $sumOfPoints + 10;
+                                      break;
+                         case 'Education': $sumOfPoints = $sumOfPoints + 10;
+                                      break;
+                         case 'Arts': $sumOfPoints = $sumOfPoints + 8;
+                                      break;
+                         case 'Free for all': $sumOfPoints = $sumOfPoints + 15;
+                                      break;            
+
+                    }        
+
+                    return $sumOfPoints;
+      
+    }
+
+
+    //kuhaon ang activities nga wala pah na attendan sa volunteer* 
+    public function getBeforeActivities(Request $request){
+
+      $volunteer_id = $request->input('volunteer_id');
+
+      $activitiesBefore = Volunteerbeforeactivity::where('volunteer_id',$volunteer_id)->get();
+
+      return response()->json($activitiesBefore);
+
+    }
+
+    //kuhaon ang activities nga na attendan nah sa volunteer
+    public function getAfterActivities(Request $request){
+      $volunteer_id = $request->input('volunteer_id');
+
+      //return dd($volunteer_id);
+      $activitiesAfter = Volunteerafteractivity::where('volunteer_id',$volunteer_id)->get();
+
+      return response()->json($activitiesAfter);
+    }
+
+    public function leaderboard(){
+
+        $volunteerLeaders = \DB::table('volunteers')->select('users.name as name', 'volunteers.*')
+                                                        ->join('users','users.user_id','=','volunteers.user_id')->orderBy('volunteers.points','desc')->get();
+
+
+        return response()->json($volunteerLeaders);
+
+    }  
+
+    public function volunteeractivitycriteria(){
+        
+    }
