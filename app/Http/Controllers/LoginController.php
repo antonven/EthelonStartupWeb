647e9237580e84bcabcd912cccfa4c34089fb6ce
kobe <kobearelativo@gmail.com>
2018-01-25 01:35:26 +0800

    asdsds

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Volunteer;
 use App\User;
+use App\Volunteerbadge;
 
 class LoginController extends Controller
 {
--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Hash;
 use App\Volunteer;
+use App\Volunteerbadge;
 
 class RegistrationController extends Controller
 {
