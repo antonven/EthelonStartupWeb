f709e9ab9e03ff3fcd2cd864f7cea96533aafa3d
kobe <kobearelativo@gmail.com>
2018-01-31 02:05:58 +0800

    asds

--- app/Volunteerbadge.php
+++ app/Volunteerbadge.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Volunteerbadge extends Model
 {
     //
+	protected $fillable = ['badge','volunteer_id','gaugeExp','star','skill','points'];
+
+	public $timestamps = false;
+	
+
 }
