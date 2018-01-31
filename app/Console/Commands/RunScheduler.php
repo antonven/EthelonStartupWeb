fb0bc84b59123d0ffd260eece27dc54461bfa9f4
kobe <kobearelativo@gmail.com>
2018-02-01 02:27:07 +0800

    schjeduler

--- app/Console/Commands/RunScheduler.php
+++ app/Console/Commands/RunScheduler.php
@@ -609,6 +609,7 @@ class RunScheduler extends Command
                                   $this->addCriteriaTotal($activities_with_false_5hrs);  
                              }
 
+
                             $activities = \DB::table('activities')->select('activities.*','foundations.name as foundation_name','foundations.foundation_id as foundation_id')
                                 ->join('foundations','foundations.foundation_id','=','activities.foundation_id')
                                 ->where('activities.status',false)
@@ -731,7 +732,7 @@ class RunScheduler extends Command
                                        'id'=>$notification_user_id,
                                        'notification_id' => $notification_id,
                                        'sender_id'=> $activity_id,
-                                       'receiver_id'=>$volunteer->volunteer_id,
+                                       'receiver_id'=>$volunteer_id,
                                        'date'=> \Carbon\Carbon::now()->format('Y-m-d h:i')
                                     ]);
 
@@ -786,19 +787,19 @@ class RunScheduler extends Command
                                                                  
                                 $newBadgePoints = $volunteerbadge->points + $criteriaTotal;
                                  $this->info('New badge points  = '.$newBadgePoints); 
-                                $volunteerbadge = Volunteerbadge::where('volunteer_id',$volunteer->volunteer_id)
-                                                                ->where('skill',$activityskill->name)
-                                                                ->update(['points'=>$newBadgePoints]);
+                                // $volunteerbadge = Volunteerbadge::where('volunteer_id',$volunteer->volunteer_id)
+                                //                                 ->where('skill',$activityskill->name)
+                                //                                 ->update(['points'=>$newBadgePoints]);
 
                                 $totalVolPoints = $volunteer->points + $criteriaTotal;
                                 $this->info('New Volunteer points  = '.$totalVolPoints);                                  
-                                Volunteer::where('volunteer_id',$volunteer->volunteer_id)->update(['points'=>$totalVolPoints]);                                 
+                               // Volunteer::where('volunteer_id',$volunteer->volunteer_id)->update(['points'=>$totalVolPoints]);                                 
                         }     
-
-                         $this->sendNotifForFiveHours($volunteer->fcm_token,$criteriaTotal,$activity_with_false_5hrs->name,$criteriaTotal,$activity_with_false_5hrs->activity_id,$newBadgePoints,$volunteer->volunteer_id);                                                   
+/*
+                         $this->sendNotifForFiveHours($volunteer->fcm_token,$criteriaTotal,$activity_with_false_5hrs->name,$criteriaTotal,$activity_with_false_5hrs->activity_id,$newBadgePoints,$volunteer->volunteer_id);        */                                           
                     }  
                      $this->info('iya nang i update to true'); 
-                     $activities_with_false_5hrs = \DB::table('activities')->select('activities.*')->update(['fiveHours'=>true]);                           
+                     //$activities_with_false_5hrs = \DB::table('activities')->select('activities.*')->update(['fiveHours'=>true]);                           
 
           }
 
