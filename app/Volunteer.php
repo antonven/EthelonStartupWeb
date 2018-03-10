ccc5c621615185efa86fe3fd282f57f35a1fa141
kobe <kobearelativo@gmail.com>
2018-03-10 13:24:46 +0800

    asds

--- app/Volunteer.php
+++ app/Volunteer.php
@@ -10,14 +10,14 @@ class Volunteer extends Model
     protected $primaryKey = 'volunteer_id';
     public $incrementing = false;
 
-    protected $fillable = ['volunteer_id','user_id','location','image_url','points','fcm_token'];
+    protected $fillable = ['volunteer_id','user_id','location','image_url','points','fcm_token','age'];
 
     public function activitiesJoined(){
-    	return $this->hasMany('App\Volunteeractivity', 'volunteer_id', 'activity_id');
+    	return $this->hasMany('App\Volunteeractivity', 'volunteer_id', 'volunteer_id');
     }
 
     public function volunteerSkills(){
-    	return $this->hasMany(Volunteerskills::class);
+    	return $this->hasMany('App\Volunteerskill','volunteer_id','volunteer_id');
     }
 
 
